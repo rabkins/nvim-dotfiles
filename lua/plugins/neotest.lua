906403a66537ff91e490eddde52721e251f3af05
Marcin Raba <rabkins@me.com>
2023-09-30 09:21:02 +0200

    config refactor: mason neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -12,11 +12,4 @@ return {
       },
     },
   },
-  -- { 'nvim-neotest/neotest-python' },
-  -- {
-  --   'nvim-neotest/neotest',
-  --   opts = {
-  --     adapters = { 'neotest-plenary' },
-  --   },
-  -- },
 }
