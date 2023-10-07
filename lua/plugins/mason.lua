e83d71524c4e4fb86a8454e8c176bfc07aeba306
Marcin Raba <rabkins@me.com>
2023-10-07 21:47:16 +0200

    mason ensure installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,7 +2,24 @@ return {
   {
     'williamboman/mason.nvim',
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { 'pyright', 'black', 'ruff' })
+      vim.list_extend(opts.ensure_installed, {
+        'bash-language-server',
+        'black',
+        'codelldb',
+        'go-debug-adapter',
+        'goimports',
+        'gopls',
+        'jsonlint',
+        'mypy',
+        'pylint',
+        'pyright',
+        'ruff',
+        'rust-analyzer',
+        'sql-formatter',
+        'sqlls',
+        'terraform-ls',
+        'yaml-language-server',
+      })
     end,
   },
 }
