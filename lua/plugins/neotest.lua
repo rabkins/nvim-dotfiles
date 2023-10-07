36ba923aa4759686b6fbcb08a8fb674973ffa1ae
Marcin Raba <rabkins@me.com>
2023-10-07 15:26:16 +0200

    neotest for rust

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,11 +4,16 @@ return {
   dependecies = {
     'nvim-neotest/neotest-python',
     'nvim-neotest/neotest-plenary',
+    'nvim-neotest/neotest-rust',
   },
   opts = {
     adapters = {
       ['neotest-python'] = {
         runner = 'pytest',
+      },
+      ['neotest-rust'] = {
+        args = { '--no-capture' },
+        dap_adapter = 'codelldb',
       },
     },
   },
