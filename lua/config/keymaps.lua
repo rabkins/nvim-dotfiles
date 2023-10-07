d00b54153584f0cd4380caf8333bfec791697fb4
Marcin Raba <rabkins@me.com>
2023-10-07 15:27:44 +0200

    zen mode keyboard shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,4 @@
--- local M = {}
---
--- M.general = {
---   n = {
---     ['C-h'] = { '<cmd> TmuxNavigatorLeft<CR>', 'window left' },
---     ['C-l'] = { '<cmd> TmuxNavigatorRight<CR>', 'window right' },
---     ['C-j'] = { '<cmd> TmuxNavigatorDown<CR>', 'window down' },
---     ['C-k'] = { '<cmd> TmuxNavigatorUp<CR>', 'window up' },
---   },
--- }
---
--- return M
+local keymap = vim.keymap.set
+
+keymap('n', '<leader>ut', '<cmd>Twilight<CR>', { desc = 'Toggle Twilight' })
+keymap('n', '<leader>uz', '<cmd>ZenMode<CR>', { desc = 'Toggle ZenMode' })
